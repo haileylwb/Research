bce00f75931927441ae9648248bc2af03562a7f4
Hailey Wong-Budiman <73897167+haileylwb@users.noreply.github.com>
2024-09-30 13:57:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -62,24 +62,47 @@ def plotPr(node_counts, probabilities):
   plt.grid()
   plt.show()
 
+
+#def estimate():
+# given two node indexes
+# take the left side of i (check if i isnt the leftmost node
+# take the right side of j (check if j isnt the rightmost node)
+# look at the inbetween
+
+# take the number of nodes time the probability of ??
+# n - the estimate for the est. number of 1's
+ 
+# if given a list with the index and values
+# iterate through the list (assume its sorted increasing order based on index)
+# calculate up to the first entry
+#
+# calculate whatever after the last entry
+
+# _ x _  _ y _ z _ _ _
+
+
+# def prMoreZeros():
+# for all the simulations count how many have more 0's
+    
   
 def main():
   p = 0.25
   s = 50000
-  k = 5 # desired index of node we want to look at
-  nodes = range(7,21)  # Node from 2 to 10
+  k = 1 # desired index of node we want to look at
+  nodes = range(2,21)  # Nodes from 7 to 20
   probabilitiesA = []
   probabilitiesB = []
 
   for n in nodes:
     sequences = createSequences(n, p, s)
     typeA, typeB = sortSequences(sequences)
-    if k < n:
+    if k < n and k > 0: # check if k is in the range of n nodes
         probabilityA = calculatePr(typeA, k)
-    probabilitiesA.append(probabilityA)
-    if k < n:
+        probabilitiesA.append(probabilityA)
         probabilityB = calculatePr(typeB, k)
-    probabilitiesB.append(probabilityB)
+        probabilitiesB.append(probabilityB)
+    else:
+        print("Out of bounds")
     #printSequence(typeA)
     #printSequence(probabilitiesA)
 
