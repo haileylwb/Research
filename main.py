7feef308214d1fb5887ec9b178065870d511e9a7
Hailey Wong-Budiman <73897167+haileylwb@users.noreply.github.com>
2024-10-02 13:57:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def printSequence(sequences):
 
 
 def calculatePr(sequence, k):
-  # how many sequences where x0 = xk , divided by number of sequences
+  # how many sequences where x0 = xk, divided by number of sequences
   count = 0
   for seq in sequence:
     if seq[0] == seq[k]:
@@ -61,78 +61,97 @@ def plotPr(node_counts, probabilities):
 def match(sequences, known):
     matchSequences = []
     for sequence in sequences:
-        match == True
+        match = True
         for node in known:
             if sequence[node['index']] != node['value']:
-                match == False
+                match = False
                 break
         if match:
             matchSequences.append(sequence)
     return matchSequences
     
     
+def estimate(sequence):
+    for seq in sequence:
+        zeroes = 0
+        l = len(seq)
+        for i in range(l):
+            if seq[i] == 0:
+                prZero = calculatePr([seq], i)
+                zeroes += (prZero * l)
+    return zeroes
+    
+    
 def sortIntervals(sequences, known):
     typeA = []
     typeB = []
+    zeroesSequence = []
     knownIndices = [node['index'] for node in known]
     
     for sequence in sequences:
+        zeroes = 0
         if knownIndices[0] > 0: # left interval
             leftInterval = sequence[:knownIndices[0]]
             if leftInterval and leftInterval[0] == leftInterval[-1]:
                 typeA.append(leftInterval)
+                zeroes += estimate(typeA)
             else:
-                typeB.append(rightInterval)
-        for i in range(len(known_indices) - 1):
+                typeB.append(leftInterval)
+                zeroes += estimate(typeB)
+                
+        for i in range(len(knownIndices) - 1):
             start = knownIndices[i]
             end = knownIndices[i + 1]
             betweenInterval = sequence[start:end]
             if betweenInterval and betweenInterval[0] == betweenInterval[-1]:
-                typeA_intervals.append(betweenInterval)
+                typeA.append(betweenInterval)
+                zeroes += estimate(typeA)
             else:
-                typeB_intervals.append(betweenInterval)
+                typeB.append(betweenInterval)
+                zeroes += estimate(typeB)
+                
         if knownIndices[-1] < len(sequence): # right interval
-            rightInterval = sequence[known_indices[-1] + 1:]
+            rightInterval = sequence[knownIndices[-1] + 1:]
             if rightInterval and rightInterval[0] == rightInterval[-1]:
                 typeA.append(rightInterval)
+                zeroes += estimate(typeA)
             else:
                 typeB.append(rightInterval)
-    return typeA, typeB
-    
+                zeroes += estimate(typeB)
+        zeroesSequence.append(zeroes)
+    return zeroesSequence
 
-#def estimate(sequence, known):
-#    count = 0
-#    for node in known:
-#    TODO
-        
-        
-def main(): # Not working yet
+  
+def main():
   p = 0.25  # Probability
   s = 50000 # Sequences generated
-  k = 1     # Desired index
-  nodes = range(2,21)
+  #k = 1     # Desired index
+  nodes = range(10,21)
   matchSequences = []
   probabilitiesA = []
   probabilitiesB = []
   
   # Nodes with values we know, using dictionary
-  knownNodes = [{'index': 1, 'value': 0}, {'index': 2, 'value': 1}, {'index': 3, 'value': 0}, {'index': 4, 'value': 1}]
+  knownNodes = [{'index': 1, 'value': 0}, {'index': 4, 'value': 1}, {'index': 6, 'value': 0}, {'index': 9, 'value': 1}]
 
   for n in nodes:
     sequences = createSequences(n, p, s)
     matchSequences = match(sequences, knownNodes)
-    typeA, typeB = sortIntervals(sequences)
-    if k < n and k > 0: # check if k is in the range of n nodes
-        probabilityA = calculatePr(typeA, k)
-        probabilitiesA.append(probabilityA)
-        probabilityB = calculatePr(typeB, k)
-        probabilitiesB.append(probabilityB)
-    else:
-        print("Out of bounds")
+    zeroesSequence = sortIntervals(matchSequences, knownNodes)
+    printSequence(zeroesSequence)
+    #print(estimate(typeA))
+    #print(estimate(typeB))
+    
+#    if k < n and k > 0: # check if k is in the range of n nodes
+#        probabilityA = calculatePr(typeA, k)
+#        probabilitiesA.append(probabilityA)
+#        probabilityB = calculatePr(typeB, k)
+#        probabilitiesB.append(probabilityB)
+#    else:
+#        print("Out of bounds")
     #printSequence(typeA)
     #printSequence(probabilitiesA)
-
-  plotPr(nodes, probabilitiesA)
+  #plotPr(nodes, probabilitiesA)
 
 
 if __name__ == "__main__":
